55a35633b9e40a55d0915514bb4fcce6cc450ee9
Gerson Janhuel <gersonjanhuelhuel@gmail.com>
2020-05-25 10:34:15 +0700

    Initial Commit

---
+++ BibleSays/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BibleSays
+//
+//  Created by Gerson Janhuel on 25/05/20.
+//  Copyright © 2020 Elcode Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
